a31c8ee6e6d614c43a777d1fc00c762c514a6f1a
Marek Zdankiewicz <m.zdankiewicz@gmail.com>
2014-10-11 19:04:16 +0200

    Vim: Simplify vim install script.

--- vim/install.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/ruby
-
-require 'fileutils'
-
-script_dir = File.expand_path(File.dirname(__FILE__))
-script_path = File.join(script_dir, "vimrc")
-
-FileUtils.ln_sf(script_path, File.join(ENV["HOME"], ".vimrc"), :verbose => true)
