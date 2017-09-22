9e75f8c7301c791b19139bab4e55e421cd243832
Marek Zdankiewicz <m.zdankiewicz@gmail.com>
2017-09-22 18:51:37 +0200

    Fish: Fix tmux pane title.

--- .config/fish/functions/tmux_pane_title.fish
+++ .config/fish/functions/tmux_pane_title.fish
@@ -1,10 +1,12 @@
 function tmux_pane_title --on-variable PWD
-  if [ $PWD = $HOME ]
-    set title '~'
-  else
-    set title (basename $PWD)
-  end
-      
-  printf "\033k%s\033\\" $title
+    if [ -n "$TMUX" ]
+        if [ $PWD = $HOME ]
+            set title '~'
+        else
+            set title (basename $PWD)
+        end
+
+        printf "\033k%s\033\\" $title
+    end
 end
 
